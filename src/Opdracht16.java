eab55225e104002cf9cad8af437e099a0a25d7c9
EdwinBolt <edwinbolt@live.nl>
2020-04-02 14:17:05 +0200

    opdracht 16 - sequence

---
+++ src/Opdracht16.java
@@ -0,0 +1,12 @@
+public class Opdracht16 {
+    public static void main(String[] args) {
+
+
+        int number = 0, step = 5;
+        for (int i = 0; i < 10; i++){
+            number += step;
+            System.out.println(number);
+            step -= 1;
+        }
+    }
+}
