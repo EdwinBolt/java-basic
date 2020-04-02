23eb045bb21f06a75d3dc18be84bb2d40d72ae3a
EdwinBolt <edwinbolt@live.nl>
2020-04-02 12:34:33 +0200

    opdracht 5 - if statements

---
+++ src/Opdracht5.java
@@ -0,0 +1,21 @@
+public class Opdracht5 {
+    public static void main(String[] args){
+
+        int x = 3, y = 5;
+
+        if(x / y == 5){
+            x = 100;
+        }
+        else if (x * y == 5){
+            x=1;
+        }
+        else if(x < y){
+            x *= x;
+        }
+        else if(x > y){
+            x += 1;
+        }
+
+
+    }
+}
